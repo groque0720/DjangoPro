af099a1968f7a02beb3a94c16baf7af889936064
Roque <gomezroque@hotmail.com>
2024-03-20 10:53:26 -0300

    staticfiles

--- miproyecto/settings/local.py
+++ miproyecto/settings/local.py
@@ -31,6 +31,7 @@ DATABASES = {
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.joinpath('media')
