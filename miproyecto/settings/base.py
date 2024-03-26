9b6af7e5ca585c3e8c160fe2f1e20992c6a7338d
Roque <gomezroque@hotmail.com>
2024-03-26 19:06:07 -0300

    allows_host

--- miproyecto/settings/base.py
+++ miproyecto/settings/base.py
@@ -28,16 +28,6 @@ SECRET_KEY = 'django-insecure-tqd#3lx7fu^v1mu!p2f1r6px_s)51yyqg082m1i+e!1d&w$bvx
 # ALLOWED_HOSTS = []
 
 
-CORS_ALLOWED_ORIGINS = [
-    "https://appdyv.online",
-    # Otros orígenes permitidos...
-]
-
-CORS_ALLOWED_ORIGINS = [
-    "https://appdyv.online",
-    # Otros orígenes permitidos...
-]
-
 # Application definition
 
 INSTALLED_APPS = [
